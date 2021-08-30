6f76e2044443ea9f9790f8f9a35c0b9530a855f0
Diogenes-Alberto <diogenes.daf@gmail.com>
2021-08-29 23:35:19 -0300

    candidatura terminada

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Comandos iniciais:
+``` bash
+  mkdir projeto_gama_final
+  cd projeto_gama_final
+  dotnet new webapi
+```
+
+
+
+
+
+
+# Comandos para poder escrever codigos js:
+``` bash
+ crie o arquivo .jshintrc
+ e escreva
+{
+    "esversion": 6
+}
+
+```
---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+
+     function teste()
+     {
+         document.getElementById("email").value=document.getElementById("dataNascimento").value;
+         console.log(document.getElementById("dataNascimento").value);
+     }
+ 
+       function mostraData()
+       {
+         let data = document.getElementById("dataNascimento").value;
+         console.log(data);
+       }
